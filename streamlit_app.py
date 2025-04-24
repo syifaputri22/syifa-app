6e26dc7c7a84265e7227511aac6ecceb215714c3
syifaputri22 <syifawanda52@gmail.com>
2025-04-24 11:29:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 syifaaa -app)
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "ayo belajar ngoding"
 )
