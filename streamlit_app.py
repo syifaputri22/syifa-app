fcb89221ea178be62857facb38a4ebddf8b3d78b
syifaputri22 <syifawanda52@gmail.com>
2025-04-24 11:08:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 syifa app)
+st.title("🎈 syifa-app)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
